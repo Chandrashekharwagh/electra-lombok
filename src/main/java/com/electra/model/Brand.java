f28ccfd71952a0066b42aa13037718ca9ad5f473
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-03 14:34:29 +0530

    commits

---
+++ src/main/java/com/electra/model/Brand.java
@@ -0,0 +1,17 @@
+package com.electra.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Brand {
+    private Long id;
+    private String name;
+    private String description;
+}
+
