a76221deade42533e22ec9c8761d3d22759f40fa
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-03 15:59:03 +0530

    commits

--- src/main/java/com/electra/model/Product.java
+++ src/main/java/com/electra/model/Product.java
@@ -1,5 +1,13 @@
 package com.electra.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Product {
     private Long id;
     private String name;
@@ -7,73 +15,4 @@ public class Product {
     private Double price;
     private String brand;
     private String supplier;
-
-    public Product(Long id, String name, String description, Double price, String brand, String supplier) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.brand = brand;
-        this.supplier = supplier;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand =  String.valueOf(brand);
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        this.supplier = String.valueOf(supplier);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", brand='" + brand + '\'' +
-                ", supplier='" + supplier + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/electra/model/Supplier.java
+++ src/main/java/com/electra/model/Supplier.java
@@ -1,48 +1,15 @@
 package com.electra.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Supplier {
     private Long id;
     private String name;
     private String contactInfo;
-
-
-    public Supplier(Long id, String name, String contactInfo) {
-        this.id = id;
-        this.name = name;
-        this.contactInfo = contactInfo;
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContactInfo() {
-        return contactInfo;
-    }
-
-    public void setContactInfo(String contactInfo) {
-        this.contactInfo = contactInfo;
-    }
-
-    @Override
-    public String toString() {
-        return "Supplier{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", contactInfo='" + contactInfo + '\'' +
-                '}';
-    }
 }
