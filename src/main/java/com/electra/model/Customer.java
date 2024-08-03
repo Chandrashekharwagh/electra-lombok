48301c254975986e5649d356529071f5d4e7b60f
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-03 14:34:39 +0530

    commits

---
+++ src/main/java/com/electra/model/Customer.java
@@ -0,0 +1,19 @@
+package com.electra.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Customer {
+    private Long id;
+    private String name;
+    private String email;
+
+
+
+}
