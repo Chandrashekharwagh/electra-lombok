9d9ced127373de8c046e213c2a50765d85524703
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-03 14:25:31 +0530

    commits

---
+++ src/main/java/com/electra/App.java
@@ -0,0 +1,13 @@
+package com.electra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
