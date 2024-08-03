49c45b26973a042caccd890c879c492803442558
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-03 14:35:04 +0530

    commits

---
+++ src/main/java/com/electra/model/Product.java
@@ -0,0 +1,19 @@
+package com.electra.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String brand;
+    private String supplier;
+}
