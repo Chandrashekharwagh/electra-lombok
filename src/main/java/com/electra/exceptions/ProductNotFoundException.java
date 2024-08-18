35ab30dc733fa008b5d90c598a55f1751698f702
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-08-18 12:56:14 +0530

    commits

---
+++ src/main/java/com/electra/exceptions/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.electra.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+        super("Product not found with id: " + id);
+    }
+}
+
